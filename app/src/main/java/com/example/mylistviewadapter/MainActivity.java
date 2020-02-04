a9aeb29fd3fc93e443826c6b3f044e235f59fb28
asadbaig4562 <baigasad015@gmail.com>
2020-02-04 17:45:44 +0500

    Initial commit

---
+++ app/src/main/java/com/example/mylistviewadapter/MainActivity.java
@@ -0,0 +1,55 @@
+package com.rp.adapterlectureone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.icu.text.IDNA;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.mylistviewadapter.Information;
+import com.example.mylistviewadapter.R;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView objectListView;
+    private String [] myData= {"Monday","Tuesday","Chuti","Thru","Friday"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initialvariable();
+        creatingadapter();
+
+    }
+    private void creatingadapter()
+    {
+        ArrayAdapter<String> objectarrayadapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,myData );
+        objectListView.setAdapter(objectarrayadapter);
+
+        objectListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int listPosition, long l) {
+
+
+                Intent intent=new Intent(MainActivity.this, Information.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initialvariable()
+    {
+        objectListView= findViewById(R.id.List_View);
+    }
+
+}
+
